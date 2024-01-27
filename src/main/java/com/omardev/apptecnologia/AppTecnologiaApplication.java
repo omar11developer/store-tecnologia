d032980427d5044f9efff6363ba7b4364663b5df
Omarmenjivardev11 <omarmenjivar083@gmail.com>
2024-01-26 21:18:03 -0600

    first commit

---
+++ src/main/java/com/omardev/apptecnologia/AppTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.omardev.apptecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTecnologiaApplication.class, args);
+	}
+
+}
